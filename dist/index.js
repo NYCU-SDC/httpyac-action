68bfb0ed585e69f3952a317d8b62c2246a3e8efc
linoil <linpinyu8@gmail.com>
2026-03-22 17:11:45 +0800

    feat: include only details for failed requests in JSON reports

--- dist/index.js
+++ dist/index.js
@@ -25720,7 +25720,7 @@ async function runHttpYacTest(journeyPath, config, outputPath, env, httpyacVersi
     for (const [key, value] of Object.entries(env || {})) {
       args.push('--var', `${key}=${value}`);
     }
-    args.push('--name', config.testcase, '--json');
+    args.push('--name', config.testcase, '--json --output none --output-failed exchange');
 
     const result = spawnSync('npx', args, {
       cwd: journeyPath,
